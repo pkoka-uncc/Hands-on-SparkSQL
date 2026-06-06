f895fd94915066c59778e03bed53529946be45a5
pkoka-uncc <pkoka@charlotte.edu>
2026-06-05 23:58:13 -0400

    Initial commit

---
+++ src/task1.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split, col
+
+# Initialize Spark Session
+spark = SparkSession.builder.appName("HashtagTrends").getOrCreate()
+
+# Load posts data
+posts_df = spark.read.option("header", True).csv("input/posts.csv")
+
+# TODO: Split the Hashtags column into individual hashtags and count the frequency of each hashtag and sort descending
+
+
+# Save result
+hashtag_counts.coalesce(1).write.mode("overwrite").csv("outputs/hashtag_trends.csv", header=True)
